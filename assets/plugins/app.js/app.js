d0048d797f377964f2b7df6bb9374958e82a2f57
devrap2497 <74716974+devrap2497@users.noreply.github.com>
2020-12-19 02:19:55 +0800

    Add files via upload

---
+++ assets/plugins/app.js/app.js
@@ -0,0 +1,15 @@
+const palindrome = (str) => {
+  /*const original = str;
+
+  const reversed = str.reversed();*/
+
+}
+
+const test = "racecar";
+
+console.log(
+  test
+  .split("")
+  .reverse("")
+  .join("")
+);
