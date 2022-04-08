4040f2ea455bf54b955922045d0bd54fa6ca7481
Rafael Aquino <rafael.aquino.18624@gmail.com>
2022-04-08 23:30:03 +0800

    Update add-expense.php

--- add-expense.php
+++ add-expense.php
@@ -95,13 +95,13 @@ if($query){
 	</div><!--/.main-->
 	
 <script src="js/jquery-1.11.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/chart.min.js"></script>
-	<script src="js/chart-data.js"></script>
-	<script src="js/easypiechart.js"></script>
-	<script src="js/easypiechart-data.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
-	<script src="js/custom.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/chart.min.js"></script>
+<script src="js/chart-data.js"></script>
+<script src="js/easypiechart.js"></script>
+<script src="js/easypiechart-data.js"></script>
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="js/custom.js"></script>
 	
 <script type="text/javascript">
         $("#success-alert").show();
